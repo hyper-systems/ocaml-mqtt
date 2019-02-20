fb65075bc10889d1c93d197b86984db8f0b5c632
Rizo Isrof <rizo@odis.io>
2019-02-20 11:37:28 +0000

    Cleanup the KA test.

--- tests/Test_keep_alive.ml
+++ tests/Test_keep_alive.ml
@@ -1,67 +1,97 @@
-module Mqtt_client = Mqtt.Mqtt.MqttClient
-module Mqtt = Mqtt.Mqtt
 let (>>=) = Lwt.bind
 let (<&>) = Lwt.(<&>)
 
-let on_listen _address ((in_channel: Lwt_io.input_channel), (out_channel: Lwt_io.output_channel)) =
-  let open Mqtt in
+module Mqtt = Mqtt.Mqtt
 
-  let ping_count = ref 0 in
-  let ping_limit = 3 in
 
-  let%lwt () = 
-    match%lwt read_packet in_channel with
-    | (_, Connect _) ->
-      let connack = Packet.Encoder.connack ~session_present:false Accepted in
-      Lwt_io.write out_channel connack
-    | _ -> Lwt.fail (Failure "Mqtt Server: Expected connect") in
+module Client = struct
+  module Mc = Mqtt.MqttClient
 
-  let rec loop () =
+  let rec read_messages stream =
+    Lwt_stream.get stream >>= function
+    | None -> Lwt_io.printl("client: message stream empty")
+    | Some (t, p) -> Lwt_io.printlf "client: got message «%s: %s»" t p >>= fun () ->
+      read_messages stream
+
+  let start ?(host = "localhost") () =
+    Lwt_io.printl "client: starting..." >>= fun () ->
+    let opt = Mc.options ~keep_alive:5 ~ping_timeout:3. ~flags:[] ~clientid:"client-1" () in
+    let%lwt client = Mc.connect ~opt host in
+    let%lwt () = Mc.subscribe client ["topic-1", Mqtt.Atmost_once] in
+    Lwt_io.printl "client: subscribed" >>= fun () ->
+    let stream = Mc.sub_stream client in
+    read_messages stream
+end
+
+
+module Server = struct
+  exception Stop
+
+  let on_listen _address (in_channel, out_channel) =
+    Lwt_io.printl "server: connection estabilished" >>= fun () ->
+    let ping_count = ref 0 in
+    let ping_limit = 3 in
+
     let%lwt () =
-      match%lwt read_packet in_channel with
-      | (_, Publish _) -> Lwt_io.printl "Publish"
-      | (_, Subscribe (id, qos_list)) ->
-        let%lwt () = Lwt_io.printl "Subscribe" in
-        let connack = suback id (List.map snd qos_list) in
+      match%lwt Mqtt.read_packet in_channel with
+      | (_, Connect _) ->
+        let connack = Mqtt.Packet.Encoder.connack ~session_present:false Accepted in
         Lwt_io.write out_channel connack
+      | _ -> Lwt.fail (Failure "server: expected connect packet") in
 
-      | (_, Disconnect) -> Lwt_io.printl "Disconnect"
-      | (_, Pingreq) when !ping_count >= ping_limit ->
-        Lwt_io.printl "Mqtt Server: Stopping PINGRESP" >>= loop
-      | (_, Pingreq) ->
-        Lwt_io.printl "Mqtt Server: Pingreq received" >>= fun () ->
-        Lwt_io.write out_channel (pingresp ()) >>= fun () ->
-        incr ping_count;
-        Lwt.return_unit
+    let rec loop () =
+      let%lwt () =
+        match%lwt Mqtt.read_packet in_channel with
+        | (_, Publish _) -> Lwt_io.printl "server: publish request"
+        | (_, Subscribe (id, qos_list)) ->
+          let%lwt () = Lwt_io.printl "server: subscribe request" in
+          let connack = Mqtt.suback id (List.map snd qos_list) in
+          Lwt_io.write out_channel connack
 
-      | _ -> Lwt.fail (Failure "Mqtt Server: Unknown packet") in
-    loop ()
-  in
-  loop ()
+        | (_, Disconnect) ->
+          let%lwt () = Lwt_io.printl "server: disconnect request" in
+          Lwt.fail Stop
+        | (_, Pingreq) when !ping_count >= ping_limit ->
+          Lwt_io.printl "server: ping request, ignoring..."
+        | (_, Pingreq) ->
+          Lwt_io.printl "server: ping request, sending response..." >>= fun () ->
+          Lwt_io.write out_channel (Mqtt.pingresp ()) >>= fun () ->
+          incr ping_count;
+          Lwt.return_unit
 
-let addr host port =
-  Lwt_unix.gethostbyname host >>= fun hostent ->
-  let inet_addr = hostent.Unix.h_addr_list.(0) in
-  Unix.ADDR_INET (inet_addr, port) |> Lwt.return
+        | _ -> Lwt.fail (Failure "server: unknown packet") in
+      loop ()
+    in
+    Lwt.catch loop
+      (function
+       | Stop -> Lwt_io.printl "server: stopped (idle connection)"
+       | exn -> Lwt.fail exn)
 
-let listen ?(host = "localhost") ?(port = 1883) () =
-  addr host port >>= fun a ->
-  Lwt_io.establish_server_with_client_address ~backlog:1000 a on_listen
 
-let rec read_client_stream stream =
-  Lwt_stream.get stream >>= function
-  | None -> Lwt_io.printl("Mqtt client: Stream finished")
-  | Some (t, p) -> Lwt_io.printlf "Mqtt client: %s: %s" t p >>= fun () ->
-    read_client_stream stream
+  let addr host port =
+    Lwt_unix.gethostbyname host >>= fun hostent ->
+    let inet_addr = hostent.Unix.h_addr_list.(0) in
+    Unix.ADDR_INET (inet_addr, port) |> Lwt.return
+
+
+  let start ?(host = "localhost") ?(port = 1883) () =
+    addr host port >>= fun a ->
+    Lwt_io.printl "server: starting..." >>= fun () ->
+    Lwt_io.establish_server_with_client_address ~backlog:1000 a on_listen
+end
+
+
+
+
+
 
 let () =
   Lwt_main.run begin
-    ignore (listen ());
-
-    Lwt_io.printl "Mqtt client: Starting subscriber" >>= fun () ->
-    let opt = Mqtt_client.options ~keep_alive:5 ~ping_timeout:3. ~flags:[] ~clientid:"client-1" () in
-    let%lwt client = Mqtt_client.connect ~opt "localhost" in
-    let%lwt () = Mqtt_client.subscribe client ["topic-1", Mqtt.Atmost_once] in
-    let stream = Mqtt_client.sub_stream client in
-    read_client_stream stream
+    match Sys.argv with
+    | [|_; "server"|] ->
+      ignore (Server.start ());
+      Lwt.pick []
+    | [|_; "client"|] -> Client.start ()
+    | _ -> Lwt_io.printlf "usage: %s <client|server>" Sys.argv.(0)
   end
+

c6e894e5f3d1bec8a526cf68f42ff1de555e34d9
Rizo Isrof <rizo@odis.io>
2020-01-31 15:28:25 +0000

    Use ppx in example. Add README.

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
-# OCaml MQTT
+# OCaml MQTT Clint
+
+This library implements the client MQTT v3 protocol.
+
+## Quickstart
+
+To install ocaml-mqtt in an esy project, add the following dependency to your package.json file:
+
+```json
+"dependencies": {
+  "ocaml-mqtt": "odis-labs/ocaml-mqtt#3a97e01"
+}
+```
+
+In your dune project add the following dependencies to your dune file:
 
 
+```lisp
+(executable
+  (name My_app)
+  (public_name my-app)
+  (libraries mqtt-client lwt)
+  (preprocess (pps lwt_ppx)))
+```
+
+## Examples
+
+Here is a basic example of a subscriber:
+
+```reason
+open Printf;
+
+let sub_example = () => {
+  let%lwt client = C.connect(~id="client-1", ~port, [host]);
+  let%lwt () = C.subscribe([("topic-1", C.Atmost_once)], client);
+  let stream = C.messages(client);
+  let rec loop = () => {
+    let%lwt msg = Lwt_stream.get(stream);
+    switch(msg) {
+      | None => Lwt_io.printl("STREAM FINISHED")
+      | Some((topic, payload)) =>
+        printlf("%s: %s", topic, payload);
+        loop()
+    };
+  };
+  loop();
+};
+```
--- examples/Basic.ml
+++ examples/Basic.ml
@@ -1,33 +1,33 @@
 module C = Mqtt_client
-let (>>=) = Lwt.bind
 
-let host = "localhsot"
+let host = "127.0.0.1"
 let port = 1883
 
 let sub_example () =
   let rec loop stream =
-    Lwt_stream.get stream >>= function
+    let%lwt msg = Lwt_stream.get stream in
+    match msg with
     | None ->
       Lwt_io.printl "STREAM FINISHED"
     | Some (t, p) ->
-      Lwt_io.printlf "%s: %s" t p >>= fun () ->
+      let%lwt () = Lwt_io.printlf "%s: %s" t p in
       loop stream
   in
-  let%lwt () = Lwt_io.printl "Starting subscriber...";
-  let%lwt client = C.connect ~id:"client-1" ~port [host];
-  C.subscribe [("topic-1", C.Atmost_once)] client >>= fun () ->
+  let%lwt () = Lwt_io.printl "Starting subscriber..." in
+  let%lwt client = C.connect ~id:"client-1" ~port [host] in
+  let%lwt () = C.subscribe [("topic-1", C.Atmost_once)] client in
   let stream = C.messages client in
   loop stream
 
 
 let pub_example () =
-  Lwt_io.printl "Starting publisher..." >>= fun () ->
-  C.connect ~id:"client-1" ~port [host] >>= fun client ->
+  let%lwt () = Lwt_io.printl "Starting publisher..." in
+  let%lwt client = C.connect ~id:"client-1" ~port [host] in
   let rec loop () =
-    Lwt_io.printl "Publishing..." >>= fun () ->
-    Lwt_io.read_line Lwt_io.stdin >>= fun line ->
-    C.publish ~qos:C.Atleast_once ~topic:"topic-1" line client >>= fun () ->
-    Lwt_io.printl "Published." >>= fun () ->
+    let%lwt () = Lwt_io.printl "Publishing..." in
+    let%lwt line = Lwt_io.read_line Lwt_io.stdin in
+    let%lwt () = C.publish ~qos:C.Atleast_once ~topic:"topic-1" line client in
+    let%lwt () = Lwt_io.printl "Published." in
     loop ()
   in
   loop ()
